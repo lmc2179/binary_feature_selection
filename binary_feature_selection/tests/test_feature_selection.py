eb4ed9d8a6757fb989e27f853af2771b35a8402d
lmc2179 <louiscialdella@gmail.com>
2018-02-25 20:42:29 -0800

    added interaction tests

--- binary_feature_selection/tests/test_feature_selection.py
+++ binary_feature_selection/tests/test_feature_selection.py
@@ -27,14 +27,26 @@ class TestInformationGainSelector(AbstractFeatureSelectorTest):
     def test_two_features(self):
         self._assert_two_features('information_gain')
 
-class TestMutualInformationInteraction(unittest.TestCase):
-    def test(self):
+class AbstractInteractionTest(unittest.TestCase):
+    def _assert_finds_interaction(self, method):
         n = 100
         x1 = np.random.randint(0, 2, n)
         x2 = np.random.randint(0, 2, n)
         useless = np.random.randint(0, 2, n)
         y = np.array(x1 * x2)
         X = np.array([x1, x2, useless]).T
-        fs = feature_selection.PairwiseInteractionSelector(method='mutual_information', max_features=1)
+        fs = feature_selection.PairwiseInteractionSelector(method=method, max_features=1)
         X_new = fs.fit_transform(X, y)
-        self.assertEqual(list(X_new[:,-1]), list(y))
+        self.assertEqual(list(X_new[:, -1]), list(y))
+
+class TestMutualInformationInteraction(AbstractInteractionTest):
+    def test_find_interaction(self):
+        self._assert_finds_interaction('mutual_information')
+
+class TestCETInteraction(AbstractInteractionTest):
+    def test_find_interaction(self):
+        self._assert_finds_interaction('cross_entropy_for_text')
+
+class TestInformationGainInteraction(AbstractInteractionTest):
+    def test_find_interaction(self):
+        self._assert_finds_interaction('information_gain')
